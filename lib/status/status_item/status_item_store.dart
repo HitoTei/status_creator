25613dacfa23dfefdf4b8a067a6d6c429b0b3f21
ttnaoto <ttnaoto@gmail.com>
2020-04-22 17:50:09 +0900

    Add: ステータスの作成
    複数のステータスの項目を追加した。これは一時的なもの。
    ステータスに振れるポイントの合計であるstatusSumPointを作成した。
    ステータスの値がマイナスにならないようにした。
    テスト追加した。

--- lib/status/status_item/status_item_store.dart
+++ lib/status/status_item/status_item_store.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:statuscreator/status/status_sum_point.dart';
 
 // ステータスの値を保持しておく
 class StatusItemStore with ChangeNotifier{
-
+  // ステータスが保持しているポイント
   int point = 0;
 
   void addPoint(){
+    if(statusSumPoint.value <= 0)return;
+
     point++;
+    statusSumPoint.value--;
+
     notifyListeners();
   }
+
   void subPoint(){
+    if(point <= 0)return;
+
+    statusSumPoint.value++;
     point--;
+
     notifyListeners();
   }
+
 }
---
+++ lib/status/status_sum_point.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+
+// Todo: グローバル変数名のは気になるので、別の方法を見つけたら変更する
+final statusSumPoint = ValueNotifier(100);
