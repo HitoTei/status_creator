c9d7d76fa77f8dbb5164199db4769c3d27be76f0
ttnaoto <ttnaoto@gmail.com>
2020-04-26 10:33:00 +0900

    Improve: UIの改善
    レイアウトを修正した。
    ポイントを増やせるようにした。

--- lib/status/status_item/status_item_widget.dart
+++ lib/status/status_item/status_item_widget.dart
@@ -1,18 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'status_item_store.dart';
 
 class StatusItemWidget extends StatelessWidget{
 
-  StatusItemWidget(String title): this._title = title;
-  final String _title;
+  StatusItemWidget(String name): this._name = name;
+  final String _name;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Row(
         children: <Widget>[
-          Text(this._title),
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: 150,
+              maxWidth: 150,
+            ),
+            child: Text(this._name),
+          ),
           _value()
         ],
       ),
@@ -40,11 +47,19 @@ class _ValueWidget extends StatelessWidget{
               onPressed: value.addPoint,
             ),
 
-            Text(value.point.toString()),
+
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                minWidth: 40,
+                maxWidth: 40,
+              ),
+              child: Text(value.point.toString()),
+            ),
 
             IconButton(
               icon: const Icon(Icons.arrow_drop_down),
               onPressed: value.subPoint,
+
             )
 
           ]
